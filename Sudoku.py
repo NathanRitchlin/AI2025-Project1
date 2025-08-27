4c9cf609aebc62c37e5ea481b5c39cc604394a7f
Nathan <nsrgy22@gmail.com>
2025-08-27 14:57:21 -0600

    File processing and puzzle checking implemented

--- Sudoku.py
+++ Sudoku.py
@@ -1,22 +1,30 @@
 import numpy
+puzzleFile = 'Puzzles/Easy-P2.txt'
+
 
 def checkPuzzle(p):
-    correctNums = [1,2,3,4,5,6,7,8,9]
 
     #checks if puzzle passes row rule
     for row in range(9):
         nums = []
         for column in range(9):
-            nums.append(int(p[row][column]))
-        if nums != correctNums:
+            nums.append(p[row][column])
+        newNums = [item for item in nums if item != "?"]
+        setNums = set(newNums)
+        if len(newNums) > len(setNums):
+            print("Fails to pass row rule")
             return False
+
 
     #checks if puzzle passes column rule
     for column in range(9):
         nums = []
         for row in range(9):
-            nums.append(int(p[row][column]))
-        if nums != correctNums:
+            nums.append(p[row][column])
+        newNums = [item for item in nums if item != "?"]
+        setNums = set(newNums)
+        if len(newNums) > len(setNums):
+            print("Fails to pass column rule")
             return False
 
     #checks if puzzle passes 3x3 grid rules
@@ -25,19 +33,32 @@ def checkPuzzle(p):
             nums = []
             for row in range(0+(i*3), (i*3)+3):
                 for column in range(0+(j*3), (j*3)+3):
-                    nums.append(int(p[row][column]))
-            if nums != correctNums:
+                    nums.append(p[row][column])
+            newNums = [item for item in nums if item != "?"]
+            setNums = set(newNums)
+            if len(newNums) > len(setNums):
+                print("Fails to pass 3x3 rule")
                 return False
 
     return True
 
+#Processes the sudoku file into a numpy array
+arr = []
+with open(puzzleFile, 'r') as file:
+    firstLine = True
+    for line in file:
+        processedLine = line.rstrip('\n')
+        if(firstLine):
+            row = processedLine[3:].split(",")
+            firstLine = False
+        else:
+            row = processedLine.split(",")
+        arr.append(row)
+puzzle = numpy.array(arr)
 
-puzzle = numpy.empty((9,9), dtype = int)
-for i in range(9):
-    for j in range(9):
-        puzzle[i][j] = i
 
-check = numpy.array()
+puzzle[6][2] = '6'
 print(puzzle)
+print("\n")
 checkPuzzle(puzzle)
 
